15215cc64225555da170b457651c6a47cc45e4fb
Jordi Portell <jordiportellmas@gmail.com>
2024-02-07 20:12:36 +0100

    Refactoring

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { iniciarJuego, renderizarTablero } from './ui';
 
-// Cuando el DOM esté cargado, se asigna la función iniciarJuego al evento click del botón de iniciar
 document.addEventListener('DOMContentLoaded', () => {
   const btnNuevaPartida = document.getElementById('btnNuevaPartida');
   if (btnNuevaPartida) {
